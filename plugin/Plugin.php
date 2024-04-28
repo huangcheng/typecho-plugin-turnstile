b81495e577891267b97b797256738da373fe79ae
HUANG Cheng <cheng@duck.com>
2024-04-29 01:24:46 +0800

    add the Turnstile widget into the login page

--- plugin/Plugin.php
+++ plugin/Plugin.php
@@ -1,31 +1,34 @@
 <?php
 
-// TODO: Replace the namespace 'Example' to your plugin name.
-namespace TypechoPlugin\Example;
+namespace TypechoPlugin\Turnstile;
 
 use Typecho\Plugin\PluginInterface;
 use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Text;
+use Typecho\Widget\Helper\Form\Element\Password;
+use Widget\Options;
 
 if (!defined('__TYPECHO_ROOT_DIR__')) {
     exit;
 }
 
 /**
- * %description%
+ * Add Cloudflare Turnstile to Your Typecho Login Form.
  *
- * @package %name%
- * @author %author%
- * @version %version%
- * @link %link%
+ * @package Turnstile
+ * @author HUANG Cheng
+ * @version 1.0.0
+ * @link https://cheng.sh
  */
 class Plugin implements PluginInterface
 {
+    public static string $Name = 'Turnstile';
     /**
      * Activate plugin method, if activated failed, throw exception will disable this plugin.
      */
     public static function activate()
     {
-        // TODO: Implement activate() method.
+        \Typecho\Plugin::factory('admin/footer.php')->begin =  __CLASS__ . '::render';
     }
 
     /**
@@ -43,7 +46,11 @@ class Plugin implements PluginInterface
      */
     public static function config(Form $form)
     {
-        // TODO: Implement config() method.
+        $siteKey = new Text('siteKey', null, null, _t('Site Key'));
+        $secretKey = new Password('secretKey', null, null, _t('Secret Key'));
+
+        $form->addInput($siteKey);
+        $form->addInput($secretKey);
     }
 
     /**
@@ -54,6 +61,31 @@ class Plugin implements PluginInterface
     public static function personalConfig(Form $form)
     {
         // TODO: Implement personalConfig() method.
+    }
+
+    public static function render(): void
+    {
+        echo '<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"></script>';
+        echo '
+            <script>
+               $(function () {               
+                    const isLoginPage = $("form[name=\"login\"]").length > 0;
+                    const sitekey = "' . Options::alloc()->plugin(Plugin::$Name)->siteKey . '";
+                    
+                    if (!isLoginPage || !sitekey) {
+                        return;
+                    }
+                    
+                    $("<div id=\"turnstile-container\"></div>").insertBefore("p.submit");
+                    
+                    turnstile.ready(function() {
+                          turnstile.render("#turnstile-container", {
+                            sitekey,
+                          });
+                    });
+              });
+            </script>
+        ';
     }
 }
 
