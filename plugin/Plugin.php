542035833329b1a9bf7713e54f7fac594576720f
HUANG Cheng <cheng@duck.com>
2024-05-01 20:30:25 +0800

    verify during the loging process

--- plugin/Plugin.php
+++ plugin/Plugin.php
@@ -3,10 +3,12 @@
 namespace TypechoPlugin\Turnstile;
 
 use Typecho\Plugin\PluginInterface;
+use Typecho\Widget;
 use Typecho\Widget\Helper\Form;
 use Typecho\Widget\Helper\Form\Element\Text;
 use Typecho\Widget\Helper\Form\Element\Password;
 use Widget\Options;
+use Widget\User;
 
 if (!defined('__TYPECHO_ROOT_DIR__')) {
     exit;
@@ -23,12 +25,15 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 class Plugin implements PluginInterface
 {
     public static string $Name = 'Turnstile';
+    public static string $TurnstileUrl = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
     /**
      * Activate plugin method, if activated failed, throw exception will disable this plugin.
      */
     public static function activate()
     {
         \Typecho\Plugin::factory('admin/footer.php')->begin =  __CLASS__ . '::render';
+
+        \Typecho\Plugin::factory('Widget_User')->loginSucceed =  __CLASS__ . '::loginSucceed';
     }
 
     /**
@@ -67,25 +72,71 @@ class Plugin implements PluginInterface
     {
         echo '<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"></script>';
         echo '
-            <script>
-               $(function () {               
-                    const isLoginPage = $("form[name=\"login\"]").length > 0;
-                    const sitekey = "' . Options::alloc()->plugin(Plugin::$Name)->siteKey . '";
-                    
-                    if (!isLoginPage || !sitekey) {
-                        return;
-                    }
-                    
-                    $("<div id=\"turnstile-container\"></div>").insertBefore("p.submit");
-                    
-                    turnstile.ready(function() {
-                          turnstile.render("#turnstile-container", {
-                            sitekey,
-                          });
-                    });
+<script>
+   $(function () {               
+        const isLoginPage = $("form[name=\"login\"]").length > 0;
+        const sitekey = "' . Options::alloc()->plugin(Plugin::$Name)->siteKey . '";
+        
+        if (!isLoginPage || !sitekey) {
+            return;
+        }
+        
+        const $submit = $("button[type=\"submit\"]");
+        
+        $submit.prop("disabled", true);
+        
+        $("<div id=\"turnstile-container\"></div>").insertBefore("p.submit");
+        
+        turnstile.ready(function() {
+              turnstile.render("#turnstile-container", {
+                sitekey,
+                language: "zh_CN",
+                callback: (token) => {
+                    $submit.prop("disabled", false);
+                }
               });
-            </script>
+        });
+  });
+</script>
         ';
+    }
+
+    public static function loginSucceed(User $user, string $username, string $password, bool $remember): void {
+        @session_start();
+
+        $cf_turnstile_response = $_REQUEST['cf-turnstile-response'];
+
+        if (!is_string($cf_turnstile_response) || strlen($cf_turnstile_response) <= 0) {
+            Widget::widget('Widget_Notice')->set('请完成人机验证', 'error');
+            $user->logout();
+
+            return;
+        }
+
+        $secret = Options::alloc()->plugin(Plugin::$Name)->secretKey;
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, Plugin::$TurnstileUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+            'secret' => $secret,
+            'response' => $cf_turnstile_response
+        ]));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        $response = json_decode($response, true);
+
+        if ($response['success'] === true) {
+            return ;
+        }
+
+        Widget::widget('Widget_Notice')->set('人机验证失败', 'error');
+        $user->logout();
     }
 }
 
